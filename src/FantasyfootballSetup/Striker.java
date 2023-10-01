629364773467a9893a9f560d71a24f80d56810e0
2022569kellen <2022569@student.cct.ie>
2023-10-01 15:32:52 +0100

    Initial commit

---
+++ src/FantasyfootballSetup/Striker.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FantasyfootballSetup;
+
+/**
+ *
+ * @author 35383
+ */
+public class Striker extends Player {
+    
+}
